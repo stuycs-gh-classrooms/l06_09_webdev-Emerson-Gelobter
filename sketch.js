05334b26270bb5bbf2af43301573e4fb004b08e7
Emerson-Gelobter <90862013+Emerson-Gelobter@users.noreply.github.com>
2022-06-03 00:09:27 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,44 @@
-function setup() {
-  // put setup code here
+int h;
+int w;
+int x;
+int y;
+int vx;
+int vy;
+
+void setup() {
+  size(1000, 500);
+  background(0);
+  fill(255);
+  w = 100;
+  h = 50;
+  x = int(random(1, width - w));
+  y = int(random(1, height - h));
+  vx = int(random(-9, 10));
+  while(vx == 0){
+    vx = int(random(-9, 10));
+  }
+  vy = int(random(-9, 10));
+  while(vy == 0){
+    vy = int(random(-9, 10));
+  }
+  rect(x, y, w, h);
 }
 
-function draw() {
-  // put drawing code here
+void draw(){
+  background(0);
+  x+=vx;
+  y+=vy;
+  if(x >= width - w){
+    vx *= -1;
+  }
+  else if(x <= 1){
+    vx *= -1;
+  }
+  if(y >= height - h){
+    vy *= -1;
+  }
+  else if(y <= 1){
+    vy *= -1;
+  }
+  rect(x, y, w, h);
 }
